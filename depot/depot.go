e4e5a6a49facdf6abbaaf4de2e4aad014a7ec44c
krusty <krusty@savla.dev>
2022-05-19 14:35:23 +0200

    commenting out depot import final output

--- depot/depot.go
+++ depot/depot.go
@@ -70,7 +70,7 @@ func PostDepotDumpRestore(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, gin.H{
 		"code": http.StatusCreated,
 		"message": "depot imported, omitting output",
-		"depots": importDepots.Depots,
+		//"depots": importDepots.Depots,
 	})
 }
 
