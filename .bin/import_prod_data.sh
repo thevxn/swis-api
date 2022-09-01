caa4aaa85977c4e5f470423353f535a8919342bd
krusty <krusty@savla.dev>
2022-09-01 19:31:08 +0200

    adding improved importing logic

--- .bin/import_prod_data.sh
+++ .bin/import_prod_data.sh
@@ -31,7 +31,7 @@ DATA_DIR=./.data
 [ ! -f "$(which jq)" ] && die "'jq' tool not found on runtime"
 
 # use explicitly POST method (-X), hide conn progress info (-s), follow locations (-L)
-alias curlp="$(which curl) -v -sLX POST"
+alias curlp="$(which curl) -sLX POST"
 alias jq="$(which jq)"
 
 #
@@ -39,18 +39,19 @@ alias jq="$(which jq)"
 # should be self-documenting
 #
 
-function import_users {
-  # import users, template:
-  #curl -d @.data/users.json -sLX POST http://${APP_URL}/users/restore | jq .
-  POST_PATH="/users/restore"
+function import_generic {
+  POST_PATH="$1"
   URL="${DEST_URL}${POST_PATH}"
 
-  DATA_FILE="${DATA_DIR}/users.json"
+  DATA_FILE="${DATA_DIR}$2"
   [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
 
-  echo "importing users..."
+  echo "importing $2..."
   curlp --data @${DATA_FILE} --url ${URL} | jq .
 }
+
+import_generic "/users/restore" "/users.json" \
+	|| die "cannot import users"
 
 function import_ssh_keys {
   # template:
@@ -76,110 +77,22 @@ function import_ssh_keys {
   done
 }
 
-function import_depots {
-  # import depot items; template:
-  #curl -d @.data/depots.json -sLX POST http://${APP_URL}/depots/restore | jq .
-  POST_PATH="/depots/restore"
-  URL="${DEST_URL}${POST_PATH}"
+import_ssh_keys
 
-  DATA_FILE="${DATA_DIR}/depots.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing/restoring/rewriting depots..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_alvax_cmd_list {
-  # import alvax command list
-  #curl -d @.data/alvax_command_list.json -sLX POST http://${APP_URL}/alvax/commands/restore | jq .
-  POST_PATH="/alvax/commands/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/alvax_command_list.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing alvax command list..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_dish_sockets {
-  POST_PATH="/dish/sockets/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/dish_sockets.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing dish sockets (socket list)..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_infrastructure {
-  POST_PATH="/infra/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/infra.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing infrastructure (hosts+networks)..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_finance {
-  POST_PATH="/finance/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/finance_accounts.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing finance (all accounts)..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_business_array {
-  POST_PATH="/business/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/business_array.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing business array..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_projects {
-  POST_PATH="/projects/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/projects.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing projects..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-function import_frontends {
-  POST_PATH="/swife/restore"
-  URL="${DEST_URL}${POST_PATH}"
-
-  DATA_FILE="${DATA_DIR}/swife_frontends.json"
-  [ ! -f "${DATA_FILE}" ] && die "DATA_FILE (${DATA_FILE}) of a no existence"
-
-  echo "importing swife frontends..."
-  curlp --data @${DATA_FILE} --url ${URL} | jq .
-}
-
-#
-# workflow/chain import
-#
-
-import_users          || die "cannot import users"
-import_ssh_keys       || die "cannot import SSH keys"
-import_depots         || die "camnot import depots"
-import_alvax_cmd_list || die "cannot import alvax command list"
-import_dish_sockets   || die "cannot import dish sockets"
-import_infrastructure || die "cannot import infrastructure"
-import_finance 	      || die "cannot import finance"
-import_business_array || die "cannot import business array"
-import_projects	      || die "cannot import projects"
-import_frontends      || die "cannot import frontends"
+import_generic "/depots/restore" "/depots.json" \
+	|| die "cannot import depots"
+import_generic "/alvax/commands/restore" "/alvax_command_list.json" \
+	|| die "cannot import alvax commands"
+import_generic "/dish/sockets/restore" "/dish_sockets.json" \
+	|| die "cannot import dish sockets"
+import_generic "/infra/restore" "/infra.json" \
+	|| die "cannot import infra"
+import_generic "/finance/restore" "/finance_accounts.json" \
+	|| die "cannot import finance accounts"
+import_generic "/business/restore" "/business_array.json" \
+	|| die "cannot import business array"
+import_generic "/projects/restore" "/projects.json" \
+	|| die "cannot import users"
+import_generic "/swife/restore" "/swife_frontends.json" \
+	|| die "cannot import swife frontends"
 
