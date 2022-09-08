d32b487c6d163ff04b4af568cc06f45f373760a8
krusty <krusty@savla.dev>
2022-09-08 14:04:27 +0200

    adding Location type to depots, spliting vast into two projects

--- depot/models.go
+++ depot/models.go
@@ -5,15 +5,33 @@ type Depots struct {
 }
 
 type Depot struct {
-	Owner      string `json:"owner_name"`
+	// Depot owner's name.
+	Owner string `json:"owner_name"`
+
+	// Generic array of depot Items.
 	DepotItems []Item `json:"depot_items"`
 }
 
 type Item struct {
-	ID          int    `json:"id"`
+	// Numeric unique ID of such Item.
+	ID int `json:"id"`
+
+	// Item descripton, name, ammount, type etc.
 	Description string `json:"desc"`
-	Misc        string `json:"misc"`
-	Location    string `json:"depot"`
+
+	// More information, e.g. the more precise location specification.
+	Misc string `json:"misc"`
+
+	// Location name of such Item.
+	Location string `json:"depot"`
+}
+
+type Location struct {
+	// Name of such location, place or depot situation.
+	Name string `json:"location_name"`
+
+	// More precise location information.
+	Misc string `json:"location_misc"`
 }
 
 // flush depots at start
