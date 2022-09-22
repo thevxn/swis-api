67bea9b6e8834aab3e5e4baf6fc2a79b36a7f84b
krusty <krusty@savla.dev>
2022-09-22 17:14:05 +0200

    add CORS support for swjango

--- helpers.go
+++ helpers.go
@@ -5,13 +5,13 @@ import gin "github.com/gin-gonic/gin"
 // https://stackoverflow.com/a/29439630
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://swjango.savla.su")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Auth-Token")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, UPDATE")
 
 		if c.Request.Method == "OPTIONS" {
-			//c.AbortWithStatus(204)
+			c.AbortWithStatus(200)
 			return
 		}
 
