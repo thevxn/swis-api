025f521c5a4453720f6614cf54074b004292ccb3
krusty <krusty@savla.dev>
2022-09-22 22:53:19 +0200

    add IoT module concept

---
+++ iot/models.go
@@ -0,0 +1,29 @@
+// internet of things (iot) module + home module.
+package iot
+
+type Things struct {
+	// IoTs' owner name.
+	Owner string `json:"owner_name"`
+
+	// Array of Things.
+	Things []Thing `json:"things"`
+}
+
+type Thing struct {
+	// Unique thing's hash ID.
+	Hash string `json:"thing_hash" binding:"required"`
+
+	// Thing's name, descriptor.
+	Name string `json:"thing_name"`
+
+	// Thing's more verbose description.
+	Description string `json:"thing_desc"`
+
+	// Type of thing (e.g. sensor)
+	Type string `json:"thing_type"`
+
+	// Target bus device (e.g. raspi01.savla.iot)
+	Bus string `json:"thing_bus"`
+}
+
+type Bus struct{}
