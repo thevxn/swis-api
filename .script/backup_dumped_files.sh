1b10ec9cc14a7d2713cdc123435071276a2e7d9e
krusty <krusty@savla.dev>
2022-10-11 10:30:30 +0200

    parametrize temporary backup report file name

--- .script/backup_dumped_files.sh
+++ .script/backup_dumped_files.sh
@@ -12,7 +12,7 @@ function die() {
 [ -z ${DUMP_DIR} ] && die "DUMP_DIR not set"
 
 BACKUP_TARGET_DIR=${DUMP_DIR}/archives
-TIMESTAMP=$(date +"%d-%m-%Y_%H-%M-%S")
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 STATUS=failure
 SIZE=""
 
@@ -26,11 +26,11 @@ tar --exclude='*.tar.gz' -czvf ${BACKUP_TARGET_DIR}/${TIMESTAMP}.tar.gz ${BACKUP
 
 # report back to swapi/backups
 SERVICE_NAME=swis-api
-TIMESTAMP=$(date +%s)
 TOKEN=i1d67db229e54a3f73047d84cc4faea850c7ed38ae3c3069c9952731ffe20744ef798feda3c559075ebc2f3f350f5dedbc56ee8a7453537d401531c32881860b0
 
 # generate backup report
-cat > /tmp/backup-report.json <<-EOF
+TMP_FILE_NAME=/tmp/swis-backup-report.json
+cat > ${TMP_FILE_NAME} <<-EOF
 {
         "service_name": "${SERVICE_NAME}",
         "last_status":  "${STATUS}",
@@ -41,7 +41,7 @@ cat > /tmp/backup-report.json <<-EOF
 EOF
 
 curl -X PUT -sL -H "X-Auth-Token: $TOKEN" \
-        --data @/tmp/backup-report.json \
+        --data @${TMP_FILE_NAME} \
         http://swapi.savla.su/backups/${SERVICE_NAME}
 
 echo "backup successfull --- ${BACKUP_TARGET_DIR}/${TIMESTAMP}.tar.gz"
