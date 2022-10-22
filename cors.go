0d67b1f05121ff7853219b2bdf0675bc915df1bc
krusty <krusty@savla.dev>
2022-10-23 00:40:11 +0200

    implement origins for CORS middleware

--- cors.go
+++ cors.go
@@ -6,20 +6,26 @@ import (
 	gin "github.com/gin-gonic/gin"
 )
 
+// https://stackoverflow.com/a/71146788
+var origins map[string]bool = map[string]bool{
+	"http://swjango.savla.su": true,
+	"http://swapp.savla.su":   true,
+}
+
 // https://stackoverflow.com/a/29439630
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://swjango.savla.su")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Auth-Token")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, UPDATE")
+		if origin := c.Request.Header.Get("Origin"); origins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Auth-Token")
 
-		if c.Request.Method == "OPTIONS" {
-			//c.AbortWithStatus(http.StatusOK)
-			c.AbortWithStatus(http.StatusNoContent)
-			return
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(http.StatusNoContent)
+				return
+			}
 		}
-
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE")
 		c.Next()
 	}
 }
