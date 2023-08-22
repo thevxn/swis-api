4e79a780fd6504dd14268df685236274519ebc87
krustowski <krusty@savla.dev>
2023-08-22 21:34:03 +0200

    upgrade go runtime to 1.21

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // @title swis-api (swapi) v5
-// @version 5.5.19
+// @version 5.5.20
 // @description sakalWeb Information System v5 RESTful API documentation
 // @termsOfService http://swagger.io/terms/
 
