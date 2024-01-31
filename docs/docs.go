f5b839ef71ec9da8799645474d90885941b6b850
krustowski <krusty@savla.dev>
2024-01-31 20:30:13 +0100

    update docs and README

--- docs/docs.go
+++ docs/docs.go
@@ -3412,7 +3412,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "5.10.0",
+	Version:          "5.10.1",
 	Host:             "swis-api-run-prod:8050",
 	BasePath:         "/",
 	Schemes:          []string{},
