19e5d435fa048c4380180068c8003a03c965c4a2
krustowski <krusty@savla.dev>
2024-02-07 04:25:27 +0100

    remove APPS_TRAEFIK_URLS constant from Makefile

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // @title swis-api (swapi) v5
-// @version 5.11.15
+// @version 5.11.16
 // @description sakalWeb Information System v5 RESTful API documentation
 // @termsOfService http://swagger.io/terms/
 
