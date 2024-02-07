77e039f22463a5fd315d13c80c2ce28c31c2e9bb
krustowski <krusty@savla.dev>
2024-02-07 03:49:34 +0000

    tune Makefile

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 // @title swis-api (swapi) v5
-// @version 5.11.17
+// @version 5.11.18
 // @description sakalWeb Information System v5 RESTful API documentation
 // @termsOfService http://swagger.io/terms/
 
