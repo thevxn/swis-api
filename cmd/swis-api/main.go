7d87c497dfe05a337857c0eaf903896290a74d72
krustowski <krusty@savla.dev>
2024-08-14 00:39:42 +0200

    update docs, update README

--- cmd/swis-api/main.go
+++ cmd/swis-api/main.go
@@ -1,5 +1,5 @@
 // @title swis-api (swapi) v5
-// @version 5.16.11
+// @version 5.17.0
 // @description sakalWeb Information System v5 RESTful API documentation
 // @termsOfService http://swagger.io/terms/
 
